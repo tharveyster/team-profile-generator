3987fbbde0af418bbdde9d41e924f0f8f433400e
tharvey <tharveyster@gmail.com>
2021-08-02 12:06:31 -0400

    Added html content to htmlGenerator.js and updated style.css

---
+++ src/htmlGenerator.js
@@ -0,0 +1,28 @@
+const createTeam = function(employees) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <header class="container container-fluid">
+            <div class="header-title">
+                <h1>Team Profile</h1>
+            </div>
+        </header>
+        <main class="container">
+            <div class="row justify-content-center">
+                ${employees}
+            </div>
+        </main>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `;
+}
